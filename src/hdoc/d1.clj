67addb0fb3c0b78fd2c3572edef9ab1061ca55dc
Maxim Tsoy <mtsoy1990@gmail.com>
2018-12-17 21:54:13 +0300

    day 1

--- src/hdoc/d1.clj
+++ src/hdoc/d1.clj
@@ -2,4 +2,24 @@
   (:require 
    [clojure.data.json :as json]))
 
-(+ 1 2)
+;Breaking the Records
+
+(defn breakingRecords [scores] 
+  (let [f (first scores)
+        r (rest scores)]
+      (loop [max f
+             min f
+             r r
+             hs 0
+             ls 0] 
+        (if (empty? r) 
+          [hs ls]
+          (let [f (first r)]
+            (cond 
+              (< max f) (recur f min (rest r) (inc hs) ls)
+              (> min f) (recur max f (rest r) hs (inc ls))
+              :else (recur max min (rest r) hs ls)))))))
+
+(assert (= [4 0] (breakingRecords [3 4 21 36 10 28 35 5 24 42])))
+(assert (= [2 4] (breakingRecords [10 5 20 20 4 5 2 25 1])))
+
