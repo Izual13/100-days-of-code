623da8d7f161df7ef9b082b71125569503b73791
Maxim Tsoy <mtsoy1990@gmail.com>
2018-12-23 20:16:16 +0300

    day 7

---
+++ src/hdoc/d7.clj
@@ -0,0 +1,26 @@
+(ns  hdoc.d7
+  (:require
+   [clojure.string :as str]))
+
+;Counting Valleys
+
+(defn countingValleys [n s]
+  (def array (into [] s))
+  (loop [s array
+         count 0
+         level 0]
+    (println "count: " count)
+    (println "level: " level)
+    (println count)
+    (if (empty? s)
+      count
+      (let [f (first s)
+            up (= f \U)]
+      (recur (rest s)  
+             (if (and (= level -1) up) (inc count) count)
+             (if up (inc level) (dec level)))))))
+
+ (countingValleys 8 "DDUUDDUDUUUD")
+
+(assert (= 1 (countingValleys 8 "UDDDUDUU")))
+(assert (= 2 (countingValleys 8 "DDUUDDUDUUUD")))
