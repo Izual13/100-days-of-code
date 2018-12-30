6f935932b3c650a317b66ca51e8ff0ada4e4b59a
Maxim Tsoy <mtsoy1990@gmail.com>
2018-12-30 13:48:51 +0300

    day 13

---
+++ src/hdoc/d13.clj
@@ -0,0 +1,16 @@
+(ns  hdoc.d13
+  (:require
+   [clojure.string :as str]))
+
+;The Hurdle Race
+
+(def abc (into {} (map-indexed (fn [x y] [y x]) "abcdefghijklmnopqrstuvwxyz")))
+
+(defn designerPdfViewer [h word]
+  (def alphabet-map (map-indexed (fn [idx itm] (nth h (get abc itm))) word))
+  (* (apply max alphabet-map) (.length word)))
+
+(designerPdfViewer [1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7] "zaba")
+
+(assert (= 9 (designerPdfViewer [1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5] "abc")))
+(assert (= 28 (designerPdfViewer [1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7] "zaba")))
