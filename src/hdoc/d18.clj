8d583240d8d1f7618f55a2ceaaf81f6a4438a8b7
Maxim Tsoy <mtsoy1990@gmail.com>
2019-01-04 22:42:06 +0300

    update day 18

--- src/hdoc/d18.clj
+++ src/hdoc/d18.clj
@@ -12,7 +12,22 @@
         (- result 1 (if (= 1 (nth c 0)) 2 0))
         (recur (+ i k) (- result 1 (if (= 1 (nth c i)) 2 0)))))))
 
-
-
 (assert (= 92 (jumpingOnClouds [0 0 1 0 0 1 1 0] 2)))
-(assert (= 92 (jumpingOnClouds [1 1 1 0 1 1 0 0 0 0] 3)))
+
+
+;Find Digits
+
+(defn findDigits [n]
+  (loop [ reminder n
+         result   0]
+    (let [x (mod reminder 10)]
+      (if (= reminder 0)
+        result
+        (recur (quot reminder 10) (+ result (if (and (not (= 0 x)) (= 0 (mod n x))) 1 0)))))))
+
+(assert (= 2 (findDigits 12)))
+(assert (= 3 (findDigits 1012)))  
+(assert (= 3 (findDigits 123456789)))
+(assert (= 3 (findDigits 114108089)))
+(assert (= 0 (findDigits 0)))
+(assert (= 5 (findDigits 106108048)))
