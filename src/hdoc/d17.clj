7707bdf8f6b85f6c02f9b4fc49414d2bae5c2ff4
Maxim Tsoy <mtsoy1990@gmail.com>
2019-01-04 21:11:30 +0300

    refactoring

--- src/hdoc/d17.clj
+++ src/hdoc/d17.clj
@@ -18,17 +18,11 @@
 (defn basic-array [l] (range 1 (inc l)))
 
 (defn permutationEquation [p]
-  (let [ 
-        mp (into {} (map-indexed (fn [index item] {item index}) p))
-        pp (println mp)
-        keys (map #(.indexOf p %)  (basic-array (count p)))
-        pp (println "keys: " keys)
-        keys (map #(.indexOf p (inc %))  keys)
-        pp (println "keys: " keys)
-        keys (map #(nth (basic-array (count p)) %)  keys)
-        pp (println "keys: " keys)
-        pp (println p)]
-    keys))
+  (->> 
+   (map #(.indexOf p %)  (basic-array (count p)))
+   (map #(.indexOf p (inc %)))
+   (map #(nth (basic-array (count p)) %))
+   ))
 
 
 (assert (= [1 3 5 4 2] (permutationEquation [4 3 5 1 2])))
