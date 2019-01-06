c79fa52568b8ae246719308c9c365121cfec7683
Maxim Tsoy <mtsoy1990@gmail.com>
2019-01-06 22:25:55 +0300

    optimization

--- src/hdoc/d20.clj
+++ src/hdoc/d20.clj
@@ -5,15 +5,10 @@
 ;Sherlock and Squares
 
 (defn squares [a b] 
-  (loop [x (Math/ceil (Math/sqrt a))         
-         result 0]
-    (let [sqr (* x x)
-          pp (println sqr)] 
-      (if (> sqr b) 
-        result 
-        (recur (inc x) (inc result))))))
-
-(squares 17 24)
+  (->> 
+   (- (Math/floor (Math/sqrt b)) (Math/ceil (Math/sqrt a)))
+   (inc)
+   (int)))
 
 (assert (= 2 (squares 3 9)))
 (assert (= 0 (squares 17 24)))
