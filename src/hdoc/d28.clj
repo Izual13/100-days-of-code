1dbac3e0ac4405c8b59dcf0873dfcbd4f0e28d3e
Maxim Tsoy <mtsoy1990@gmail.com>
2019-01-15 23:42:02 +0300

    optimization part 2

--- src/hdoc/d28.clj
+++ src/hdoc/d28.clj
@@ -4,41 +4,33 @@
 
 ;ACM ICPC Team
 
-
-(defn calc [a1 a2]
-  (let [length (count a1)]
-    (reduce + (for [i (range length)] (if (or (= (get a1 i) \1) (= \1 (get a2 i))) 1 0)))))
-
 (defn maxCount [s]
   (loop [max 0
          count 0
          s (seq s)]
-    (if (empty? s) 
+    (if (empty? s)
       [max count]
       (let [f     (first s)
-            count (cond 
-                    (= max f) (inc count) 
+            count (cond
+                    (= max f) (inc count)
                     (> f max) 1
                     :else  count)
             max   (if (> f max) f max)]
+
         (recur max count (next s))))))
 
 
-(get (boolean-array (map #(if (= % \1) true false) "1011")) 0)
-
-
 (defn acmTeam [topic]
-  (maxCount (let [n (count topic)]
-    (for [x     (range n)
-          y     (range n)
-          :when (and (not= x y) (< x y))] 
-      (calc (nth topic x) (nth topic y))
-      ))))
-
-(count [1 2 3])
+  (maxCount (let [n (count topic)
+                  topic (vec (map #(BigInteger. % 2)  topic))]
+              (for [x     (range n)
+                    y     (range n)
+                    :when (and (not= x y) (< x y))]
+                (let [intersection (.or (get topic x) (get topic y))]
+                  (.bitCount intersection))))))
 
 (acmTeam ["11101" "10101" "11001" "10111" "10000" "01110"])
 
-
 (assert (= [5 6] (acmTeam ["11101" "10101" "11001" "10111" "10000" "01110"])))
 (assert (= [5 2] (acmTeam ["10101" "11100" "11010" "00101"])))
+
