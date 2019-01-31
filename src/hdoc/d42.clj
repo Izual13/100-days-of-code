cf229285f49c976f07ab921cfbd9e9b60757c7a2
Maxim Tsoy <mtsoy1990@gmail.com>
2019-01-31 22:05:48 +0300

    deque part1

--- src/hdoc/d42.clj
+++ src/hdoc/d42.clj
@@ -18,13 +18,48 @@
 (let [deque (deque 5)]
   (.addFirst deque 1)
   (.addFirst deque 6)
-  (.addFirst deque 50)
-  (.getLast deque))
+  ; (.addFirst deque 50)
+  (.peekLast deque)
+  (.peekLast deque)
+  (.peekLast deque)
+  (.peekLast deque)
+  (.peekLast deque)
+  (print-deque deque)
+  )
 
+(defn print-deque [d] 
+  (println "print-deque ->")
+  (loop [first (.peekFirst d)]
+    (if (nil? first) ""
+        (let [] (println first)
+             (.removeFirst d)
+             (recur (.peekFirst d))
+             )))
+  (println "<- print-deque")
+  )
 
-(defn windowMax [n k])
+(defn windowMax [n k]
+  (let [deque (deque k)]
+    (doseq [i (range k)] 
+      (let [last  (get n (.peekLast deque))
+            value (get n i)]
+        (println "last: " last "; value: " value)
+        (if (or (nil? last) (> last value))
+          (.addLast deque i)
+          (do (.clear deque)
+              (.addLast deque i))          )))
+    
+    (print-deque deque)
+    
+    (doseq [i (range k (count n))] 
+      (println (get n i)))
+    ))
 
 (windowMax [10 5 2 7 8 7] 3)
+
+
+(doseq [x (range 10)]
+  (println x))
 
 (assert (= [10 7 8 8] (windowMax [10 5 2 7 8 7] 3)))
 
