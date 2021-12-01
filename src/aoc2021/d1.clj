b910e000397313444a3acd3c1e56658632e1be5e
Maxim Tsoy <mtsoy1990@gmail.com>
2021-12-01 09:23:09 +0300

    added day1 part2

--- src/aoc2021/d1.clj
+++ src/aoc2021/d1.clj
@@ -17,3 +17,27 @@
 
 (count-measurement-increases input-from-file)
 
+
+
+(defn three-measurement 
+  ([input] (let [e1 (nth input 0) e2 (nth input 1) e3 (nth input 2)] (three-measurement e1 e2 e3 (nthrest input 3) (+ e1 e2 e3) 0)))
+  ([e1 e2 e3 input sum result]
+   ;(println e1 e2 e3 input sum result)
+   (if (empty? input)
+       (if (> (+ e1 e2 e3) sum) (inc result) result)
+       (if (> (+ e1 e2 e3) sum)
+         (three-measurement e2 e3 (first input) (rest input) (+ e1 e2 e3) (inc result))
+         (three-measurement e2 e3 (first input) (rest input) (+ e1 e2 e3) result)))))
+
+
+
+(three-measurement input-from-file)
+
+
+(three-measurement [199 200 208 210 200 207 240 269 260 263])
+
+(nthrest input-from-file 3)
+
+(nth input-from-file 2)
+
+(+ 269 260 263)
