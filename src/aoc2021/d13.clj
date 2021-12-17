08d8217f599c4baeac43e83635a18367039314e2
Maxim <mtsoy1990@gmail.com>
2021-12-17 03:02:18 +0300

    added day13 part1

--- src/aoc2021/d13.clj
+++ src/aoc2021/d13.clj
@@ -2,13 +2,13 @@
   (:require [clojure.string :as str]))
 
 
+
 (def input-from-file-test
-  (let [[paper insrruction] (str/split (slurp "resources/aoc2021/day13_t") #"\n\n")
+  (let [[paper insrruction] (str/split (slurp "resources/aoc2021/day13_t") #"\r\n\r\n")
         parsed-paper (->> paper
                           (str/split-lines)
                           (mapv #(str/split % #","))
                           (mapv #(mapv (fn [x] (Long/parseLong x)) %)))
-
         parsed-insrructions (->> insrruction
                                  (str/split-lines)
                                  (mapv #(let [[_ c p] (re-matches #"fold along (\w+)=(\d+)" %)] [c (Long/parseLong p)])))]
@@ -19,15 +19,14 @@
 
 
 (def input-from-file
-  (let [[paper insrruction] (str/split (slurp "resources/aoc2021/day13_1") #"\n\n")
+  (let [[paper insrruction] (str/split (slurp "resources/aoc2021/day13_1") #"\r\n\r\n")
         parsed-paper (->> paper
                           (str/split-lines)
                           (mapv #(str/split % #","))
                           (mapv #(mapv (fn [x] (Long/parseLong x)) %)))
-
         parsed-insrructions (->> insrruction
                                  (str/split-lines)
-                                 (mapv #(let [[_ c p] (re-matches #"fold along (\w+)=(\d+)" %)] [c p])))]
+                                 (mapv #(let [[_ c p] (re-matches #"fold along (\w+)=(\d+)" %)] [c (Long/parseLong p)])))]
     {:points parsed-paper :insrructions parsed-insrructions}))
 
 (defn get-max [m]
@@ -38,41 +37,55 @@
 
 (defn build-matrix [points]
   (let [[maxx maxy] (get-max points)
-        _ (println maxx maxy)
         result (vec (for [_ (range maxy)] (vec (for [_ (range maxx)] false))))
-        ;; _ (println "result:" result)
         result (loop [p points r result]
-                ;;  (println "p" (first p))
                  (if (empty? p)
                    r
                    (recur (rest p) (assoc-in r (reverse (first p)) true))))]
     {:x maxx :y maxy :matrix result}))
 
 (defn fold-matrix [m [d p]]
-  (println d p)
-  ;(println "(get-in m [y x])" (get-in m [11 1]))
-  (if (= d "y") 
+  (if (= d "y")
     (let [matrix (:matrix m)
-          
-          sharps (for [y (range p (:y m)) 
+
+          sharps (for [y (range p (:y m))
                        x (range 0 (:x m))
                        :when (get-in matrix [y x])]
-            [y x])
-          _ (println "sharps" sharps)
-          ] m) 
-    m))
+                   [y x])
+          new-matrix (loop [s sharps r matrix]
+                       (if (empty? s)
+                         r
+                         (recur (rest s) (assoc-in r [(- (* 2 p) (ffirst s)) (second (first s))] true))))] {:x (:x m) :y p :matrix new-matrix})
+    (let [matrix (:matrix m)
+          _ (println (type (:y m)) (type (:x m)) (type p))
+
+          sharps (for [y (range 0 (:y m))
+                       x (range p (:x m))
+                       :when (get-in matrix [y x])]
+                   [y x])
+          new-matrix (loop [s sharps r matrix]
+                       (if (empty? s)
+                         r
+                         (recur (rest s) (assoc-in r [(ffirst s) (- (* 2 p) (second (first s)))] true))))] {:x p :y (:x m) :matrix new-matrix})))
 
 (defn part1 [input]
   (let [matrix (build-matrix (:points input))
-        new-matrix (fold-matrix matrix (first (:insrructions input)))
-        _ (println (map println (:matrix new-matrix)))]
-    matrix))
-
-(assert (= 10 (part1 input-from-file-test)))
-(assert (= 4970 (part1 input-from-file)))
+        new-matrix (fold-matrix matrix (first (:insrructions input)))]
+    (count (for [y (range 0 (:y new-matrix))
+                 x (range 0 (:x new-matrix))
+                 :when (get-in (:matrix new-matrix) [y x])] true))))
 
 
-(defn part2 [input])
+(assert (= 17 (part1 input-from-file-test)))
+(assert (= 647 (part1 input-from-file)))
+
+
+(defn part2 [input]
+  (let [matrix (build-matrix (:points input))
+        new-matrix (fold-matrix matrix (first (:insrructions input)))]
+    (count (for [y (range 0 (:y new-matrix))
+                 x (range 0 (:x new-matrix))
+                 :when (get-in (:matrix new-matrix) [y x])] true))))
 
 (assert (= 36 (time (part2 input-from-file-test))))
 (assert (= 137948 (time (part2 input-from-file))))
