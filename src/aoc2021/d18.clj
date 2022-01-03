879bd1276082dc36c3d1663228a532bd0342303c
Maxim <mtsoy1990@gmail.com>
2022-01-03 09:37:26 +0300

    day18 part1

--- src/aoc2021/d18.clj
+++ src/aoc2021/d18.clj
@@ -75,7 +75,7 @@
 (assert (= [0] (first-i [7,[6,[5,[4,[3,2]]]]])))
 
 
-(defn update-snails [snails path]
+(defn explode-snails [snails path]
   ;; (println "update-snails")
   (let [r-path (pop path)
         ;; ppath (peek path)
@@ -98,16 +98,33 @@
     ;;     (assoc-in l (+ (left c) (get-in snails l 0))))
     new-snails))
 
-(pop [1 2 3])
+(defn split-snails [snails]
+  ;;(println "split-snails" snails)
+  (loop [s (first-i snails) r snails changed false]
+    (if (nil? s) [r changed]
+        (let [v (get-in r s)
+              ni (next-i r s)]
+          (if (> v 9)
+            (recur ni (assoc-in r s [(int (/ v 2)) (int (Math/ceil (/ v 2)))]) true)
+            (recur ni r changed))))))
+
+
+(assert (= 2 (int (/ 5 2))))
+(assert (= 3 (int (Math/ceil (/ 5 2)))))
+(assert (= [[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]] (left (split-snails [[[[0,7],4],[15,[0,13]]],[1,1]]))))
 
 (defn normalization [snails]
+  (println (left snails))
+  (println "+" (right snails) "\n\n")
   (loop [stack (first-i snails) result snails]
     ;;(println "stack" stack "value" (get-in result stack))
     (if (nil? stack)
-      result
+      (let [[new-result changed] (split-snails result)]
+        (if (false? changed)
+          result
+          (recur (first-i new-result) new-result)))
       (if (> (count stack) 4)
-        (let [new-result  (update-snails result stack)
-              ;; _ (println "new-result" new-result "current stack" stack)
+        (let [new-result  (explode-snails result stack)
               new-stack (next-i new-result (pop stack))]
           (recur new-stack new-result))
         (recur (next-i result stack) result)))))
@@ -117,14 +134,35 @@
 (assert (= [7,[6,[5,[7,0]]]] (normalization [7,[6,[5,[4,[3,2]]]]])))
 (assert (= [[6,[5,[7,0]]],3] (normalization [[6,[5,[4,[3,2]]]],1])))
 (assert (= [[3,[2,[8,0]]],[9,[5,[7,0]]]] (normalization [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])))
+(assert (= [[[[0,7],4],[[7,8],[6,0]]],[8,1]] (normalization [[[[[4,3],4],4],[7,[[8,4],9]]] [1,1]])))
+
+
+(defn calc [snails] 
+  (+ 
+   (* 3 (if (vector? (left snails))
+          (calc (left snails))
+          (left snails)))
+   (* 2 (if (vector? (right snails))
+          (calc (right snails))
+          (right snails)))))
+
+(assert (= 29 (calc [9,1])))
+(assert (= 129 (calc [[9,1],[1,9]])))
+(assert (= 3488 (calc [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]])))
 
 (defn part1 [input]
-  (reduce #(normalization [%1 %2]) input))
+  (->> input
+       (reduce #(normalization [%1 %2]))
+       (calc)))
 
-(assert (= [[1,2],[[3,4],5]] (part1 `([1,2] [[3,4],5]))))
+(assert (= 143 (time (part1 [[1,2],[[3,4],5]]))))
+(assert (= 1384 (time (part1 [[[[0,7],4],[[7,8],[6,0]]],[8,1]]))))
+(assert (= 445 (time (part1 [[[[1,1],[2,2]],[3,3]],[4,4]]))))
+(assert (= 791 (time (part1 [[[[3,0],[5,3]],[4,4]],[5,5]]))))
+(assert (= 1137 (time (part1 [[[[5,0],[7,4]],[5,5]],[6,6]]))))
+(assert (= 3488 (time (part1 [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]))))
 
-
-(assert (= 45 (time (part1 input-from-file-test))))
+(assert (= 3488 (time (part1 input-from-file-test))))
 (assert (= 2850 (time (part1 input-from-file))))
 
 
@@ -134,3 +172,4 @@
 
 (assert (= 112 (time (part2 input-from-file-test))))
 (assert (= 1117 (time (part2 input-from-file))))
+
