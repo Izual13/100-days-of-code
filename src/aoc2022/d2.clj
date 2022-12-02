019d2ba4006835b57aa1785a9bab927b21ab30ba
Maxim Tsoy <mtsoy1990@gmail.com>
2022-12-02 08:44:35 +0300

    day 2

---
+++ src/aoc2022/d2.clj
@@ -0,0 +1,36 @@
+(ns  aoc2022.d2
+  (:require [clojure.string :as str]))
+
+(defn parse-round [s]
+  (let [[_ a b] (re-matches #"(.) (.)" s)]
+    [a b]))
+
+
+(def test-rounds (map parse-round (clojure.string/split (slurp "resources/aoc2022/day2_t") #"\n")))
+
+(def rounds (map parse-round (clojure.string/split (slurp "resources/aoc2022/day2_1") #"\n")))
+
+;;Rock (A, X) Paper(B, Y) Scissors(C, Z)
+
+(defn calc [[a b]] 
+  (let[b (cond
+           (= b "X") "A"
+           (= b "Y") "B"
+           (= b "Z") "C"
+           :else b)
+       f (cond
+           (= b "A") 1
+           (= b "B") 2
+           :else 3)
+       l (cond
+           (= a b) 3
+           (and (= a "C") (= b "A")) 6
+           (and (= a "A") (= b "B")) 6
+           (and (= a "B") (= b "C")) 6
+           :else 0)
+       ] (+ f l)))
+
+(assert (= 15 (apply + (map calc test-rounds))))
+
+(assert (= 10595 (apply + (map calc rounds))))
+
