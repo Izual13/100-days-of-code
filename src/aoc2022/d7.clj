15d33aa2299b5d018f3ce24386c36062343b4f4d
Maxim Tsoy <mtsoy1990@gmail.com>
2022-12-07 14:32:03 +0300

    day7 part 2

--- src/aoc2022/d7.clj
+++ src/aoc2022/d7.clj
@@ -14,13 +14,11 @@
 (def input (clojure.string/split (slurp "resources/aoc2022/day7_1") #"\$ "))
 
 
-(map parse-input (filter not-empty test-input))
-
 (defn parse-file [f]
   (let [[_ size name] (re-matches #"(\d*) (.*)" f)]
     {:s (Long/parseLong size) :n name}))
 
-(parse-file "14848514 b.txt")
+(assert (= {:s 14848514, :n "b.txt"} (parse-file "14848514 b.txt")))
 
 (defn parse-files [files] 
   (loop [f files r []] 
@@ -30,18 +28,15 @@
         (recur (rest f) r)
         (recur (rest f) (conj r (parse-file (first f))))))))
 
-(parse-files `("dir a" "14848514 b.txt" "8504156 c.dat" "dir d"))
+(assert (= {:files [{:s 14848514, :n "b.txt"} {:s 8504156, :n "c.dat"}]} (parse-files `("dir a" "14848514 b.txt" "8504156 c.dat" "dir d"))))
 
 
   
 (defn build-dirs [commands] 
   (loop [c commands r {} s []] 
-    ;(println r)
     (if (empty? c) 
       r
-      (let [[i o] (first c)
-            ;_ (println i o)
-            ]
+      (let [[i o] (first c)]
         (cond
           (= i "cd ..") (recur (rest c) r (pop s))
           (str/starts-with? i "cd ") (recur (rest c) r (conj s (subs i 3)))
@@ -57,34 +52,28 @@
 (assert (= 0 (get-size-directory {})))
 
 
-
 (defn calc-directories
   ([d] (calc-directories d ["/"]))
   ([d s] 
-   (println "bd" d s)
    (let [new-d (get-in d s)
-         _ (println (keys (dissoc new-d :files)))
          children (map (fn [k] (calc-directories d (conj s k))) (keys (dissoc new-d :files)))
-         _ (println "children" children)
          size (get-size-directory new-d)]
-     {:s (+ size (reduce + (map :s children))) :c (vec children) :d s})))
+     {:s (+ size (reduce + (map :s children))) :c (vec children)})))
 
 
 
-(calc-directories {"/"
-                   {:files [{:s 14848514, :n "b.txt"} {:s 8504156, :n "c.dat"}],
-                    "a" {:files [{:s 29116, :n "f"} {:s 2557, :n "g"} {:s 62596, :n "h.lst"}], "e" {:files [{:s 584, :n "i"}]}},
-                    "d" {:files [{:s 4060174, :n "j"} {:s 8033020, :n "d.log"} {:s 5626152, :n "d.ext"} {:s 7214296, :n "k"}]}}}
-  )
-
+(assert (= {:s 48381165, :c [{:s 94853, :c [{:s 584, :c []}]} {:s 24933642, :c []}]} 
+          (calc-directories {"/"
+                             {:files [{:s 14848514, :n "b.txt"} {:s 8504156, :n "c.dat"}],
+                              "a" {:files [{:s 29116, :n "f"} {:s 2557, :n "g"} {:s 62596, :n "h.lst"}], "e" {:files [{:s 584, :n "i"}]}},
+                              "d" {:files [{:s 4060174, :n "j"} {:s 8033020, :n "d.log"} {:s 5626152, :n "d.ext"} {:s 7214296, :n "k"}]}}})))
 
 (defn calc-total [d] 
-  (println d)
   (if (empty? d)
     0
     (let [s (:s d)
           s (if (> s 100000) 0 s)
-          r (reduce + (map calc (:c d)))
+          r (reduce + (map calc-total (:c d)))
           ] (+ s r))))
 
 (assert (= 95437 (calc-total {:s 48381165, :c [{:s 94853, :c [{:s 584, :c []}]} {:s 24933642, :c []}]})))
@@ -107,6 +96,39 @@
 
 
 
+(defn sorted-used-space [d]  
+  (if 
+    (empty? d)
+    [0]
+    (let [s (:s d)
+          r (mapcat sorted-used-space (:c d))] 
+      (sort (conj r s)))))
+
+(defn calc-enough-space [m] 
+  (let [used (last m)
+        total 70000000
+        free (- total used)
+        needed (- 30000000 free)]
+    (->> m
+      (filter #(> % needed))
+      first)))
+
+
+(assert (= 24933642 (->> test-input
+                      (filter not-empty)
+                      (map parse-input)
+                      build-dirs
+                      calc-directories
+                      sorted-used-space
+                      calc-enough-space)))
+
+(assert (= 7421137 (->> input
+                     (filter not-empty)
+                     (map parse-input)
+                     build-dirs
+                     calc-directories
+                     sorted-used-space
+                     calc-enough-space)))
 
 
 (comment 
