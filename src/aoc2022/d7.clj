aac54a5929a7b81e94edbab999f4b6a0ae8869ff
Maxim Tsoy <mtsoy1990@gmail.com>
2022-12-07 13:36:01 +0300

    day 7 part 1

---
+++ src/aoc2022/d7.clj
@@ -0,0 +1,120 @@
+(ns aoc2022.d7
+  (:require 
+    [clojure.string :as str]
+    [clj-async-profiler.core :as prof]))
+
+
+(defn parse-input [s] 
+  (let [l (clojure.string/split s #"\n")
+        c (first l)
+        o (rest l)] [c o]))
+
+
+(def test-input (clojure.string/split (slurp "resources/aoc2022/day7_t") #"\$ "))
+(def input (clojure.string/split (slurp "resources/aoc2022/day7_1") #"\$ "))
+
+
+(map parse-input (filter not-empty test-input))
+
+(defn parse-file [f]
+  (let [[_ size name] (re-matches #"(\d*) (.*)" f)]
+    {:s (Long/parseLong size) :n name}))
+
+(parse-file "14848514 b.txt")
+
+(defn parse-files [files] 
+  (loop [f files r []] 
+    (if (empty? f) 
+      {:files r}
+      (if (str/starts-with? (first f) "dir") 
+        (recur (rest f) r)
+        (recur (rest f) (conj r (parse-file (first f))))))))
+
+(parse-files `("dir a" "14848514 b.txt" "8504156 c.dat" "dir d"))
+
+
+  
+(defn build-dirs [commands] 
+  (loop [c commands r {} s []] 
+    ;(println r)
+    (if (empty? c) 
+      r
+      (let [[i o] (first c)
+            ;_ (println i o)
+            ]
+        (cond
+          (= i "cd ..") (recur (rest c) r (pop s))
+          (str/starts-with? i "cd ") (recur (rest c) r (conj s (subs i 3)))
+          (= i "ls") (recur (rest c) (assoc-in r s (parse-files o)) s)
+          ;(assoc-in r (conj s (subs i 3)) {:dir true})
+          :else (recur (rest c) r s))))))
+
+
+
+(defn get-size-directory [d] (reduce + (map #(:s %) (:files d))))
+
+(assert (= 30 (get-size-directory {:files [{:s 10} {:s 20}]})))
+(assert (= 0 (get-size-directory {})))
+
+
+
+(defn calc-directories
+  ([d] (calc-directories d ["/"]))
+  ([d s] 
+   (println "bd" d s)
+   (let [new-d (get-in d s)
+         _ (println (keys (dissoc new-d :files)))
+         children (map (fn [k] (calc-directories d (conj s k))) (keys (dissoc new-d :files)))
+         _ (println "children" children)
+         size (get-size-directory new-d)]
+     {:s (+ size (reduce + (map :s children))) :c (vec children) :d s})))
+
+
+
+(calc-directories {"/"
+                   {:files [{:s 14848514, :n "b.txt"} {:s 8504156, :n "c.dat"}],
+                    "a" {:files [{:s 29116, :n "f"} {:s 2557, :n "g"} {:s 62596, :n "h.lst"}], "e" {:files [{:s 584, :n "i"}]}},
+                    "d" {:files [{:s 4060174, :n "j"} {:s 8033020, :n "d.log"} {:s 5626152, :n "d.ext"} {:s 7214296, :n "k"}]}}}
+  )
+
+
+(defn calc-total [d] 
+  (println d)
+  (if (empty? d)
+    0
+    (let [s (:s d)
+          s (if (> s 100000) 0 s)
+          r (reduce + (map calc (:c d)))
+          ] (+ s r))))
+
+(assert (= 95437 (calc-total {:s 48381165, :c [{:s 94853, :c [{:s 584, :c []}]} {:s 24933642, :c []}]})))
+
+
+(assert (= 95437 (->> test-input
+                   (filter not-empty)
+                   (map parse-input)
+                   build-dirs
+                   calc-directories
+                   calc-total)))
+
+(assert (= 1334506 (->> input
+                     (filter not-empty)
+                     (map parse-input)
+                     build-dirs
+                     calc-directories
+                     calc-total)))
+
+
+
+
+
+
+(comment 
+  (do
+    (vec (for [i (range 1000)] (optimized-find-marker input 4)))
+    (println "start profiling")
+    (prof/start)
+    (optimized-find-marker input 4)
+    (println (prof/stop))
+    (println "end profiling"))
+  )
