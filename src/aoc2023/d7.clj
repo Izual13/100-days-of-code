0bb8383fecc06fbb406171d1c2a67455a31fce68
Maxim Tsoy <mtsoy1990@gmail.com>
2023-12-07 13:11:27 +0600

    day 6 part 2

--- src/aoc2023/d7.clj
+++ src/aoc2023/d7.clj
@@ -25,38 +25,47 @@
 
 (compare 1 2)
 
-;второе больше
 (defn compare-puzzle [[f _] [s _]] 
-  (println f s)
   (let [ff (sort compare-frequencies (frequencies f))
         sf (sort compare-frequencies (frequencies s))
-        _ (println ff sf)
-        _ (println (last ff) (last sf))
-        ;_ (println (> (second (last ff)) (second (last sf))))
         r (cond 
             (> (second (last ff)) (second (last sf))) 1
             (< (second (last ff)) (second (last sf))) -1
-            (> (get cards (first (last ff))) (get cards (first (last sf)))) 1
-            (< (get cards (first (last ff))) (get cards (first (last sf)))) -1
+            (< (count ff) (count sf)) 1
+            (> (count ff) (count sf)) -1
+            (> (get cards (nth f 0)) (get cards (nth s 0))) 1
+            (< (get cards (nth f 0)) (get cards (nth s 0))) -1
+            
+            (> (get cards (nth f 1)) (get cards (nth s 1))) 1
+            (< (get cards (nth f 1)) (get cards (nth s 1))) -1
+            
+            
+            (> (get cards (nth f 2)) (get cards (nth s 2))) 1
+            (< (get cards (nth f 2)) (get cards (nth s 2))) -1
+            
+            
+            (> (get cards (nth f 3)) (get cards (nth s 3))) 1
+            (< (get cards (nth f 3)) (get cards (nth s 3))) -1
+            
+            (> (get cards (nth f 4)) (get cards (nth s 4))) 1
+            (< (get cards (nth f 4)) (get cards (nth s 4))) -1
+            
+            (> (get cards (nth f 5)) (get cards (nth s 5))) 1
+            (< (get cards (nth f 5)) (get cards (nth s 5))) -1
             :else 0
-            )
-        _ (println "result:" r)]
+            )]
     
     r))
 
-(compare-puzzle ["QQQJA" 483] ["KTJJT" 220])
+(assert (= `(["2AAAA" 2] ["33332" 1]) (sort compare-puzzle [["33332" 1] ["2AAAA" 2]])))
+(assert (= `(["KJJJT" 2] ["23332" 1]) (sort compare-puzzle [["23332" 1] ["KJJJT" 2]])))
+(assert (= `(["KTJJT" 2] ["QQQJA" 1]) (sort compare-puzzle [["QQQJA" 1] ["KTJJT" 2]])))
+(assert (= `(["T55J5" 2] ["QQQJA" 1]) (sort compare-puzzle [["QQQJA" 1] ["T55J5" 2]])))
+(assert (= `(["KTJJT" 2] ["KK677" 1]) (sort compare-puzzle [["KK677" 1] ["KTJJT" 2]])))
+(assert (= `(["KK677" 1] ["T55J5" 2]) (sort compare-puzzle [["KK677" 1] ["T55J5" 2]])))
 
-(compare-puzzle ["QQQJA" 483] ["T55J5" 684])
 
-(compare-puzzle ["KK677" 28] ["KTJJT" 220])
 
-(compare-puzzle ["KK677" 28] ["T55J5" 684])
-
-(compare 1 2)
-
-(get cards (first [\Q 3]))
-
-(max-by second {T 1, 5 3, J 1})
 
 (println "\n\n\n\n")
 
@@ -66,23 +75,17 @@
       r
       (recur (next s) (inc i) (+ r (* i (first s)))))))
 
-(->> puzzles-t
-  (map parse-puzzle)
-  (sort compare-puzzle)
-  (map second)
-  calc)
+(assert (= 6440 (->> puzzles-t
+                  (map parse-puzzle)
+                  (sort compare-puzzle)
+                  (map second)
+                  calc)))
 
-(->> puzzles
-  (map parse-puzzle)
-  (sort compare-puzzle)
-  (map second)
-  calc)
-
-
-;;; tried 249600020
-
-
-(["32T3K" 765] ["KTJJT" 220] ["KK677" 28] ["T55J5" 684] ["QQQJA" 483])
+(assert (= 249748283 (->> puzzles
+                       (map parse-puzzle)
+                       (sort compare-puzzle)
+                       (map second)
+                       calc)))
 
 
 
@@ -91,5 +94,76 @@
 
 
 
+;;;;;part 2
 
+(def cards-v2 {\A 14, \K 13, \Q 12, \J 1, \T 10, \9 9, \8 8, \7 7, \6 6, \5 5, \4 4, \3 3, \2 2})
+
+(defn compare-frequencies-v2 [[w1 c1] [w2 c2]] 
+  (cond 
+    (> c1 c2) 1
+    (< c1 c2) -1
+    :else (compare (get cards-v2 w1) (get cards-v2 w2))))
+
+(defn compare-puzzle-v2 [[f _] [s _]] 
+  (let [ff (frequencies f)
+        sf (frequencies s)
+        j-ff (get ff \J)
+        j-sf (get sf \J)
+        
+        ff (dissoc ff \J)
+        sf (dissoc sf \J)
+        
+        sff (sort compare-frequencies-v2 ff)
+        ssf (sort compare-frequencies-v2 sf)
+               
+        ff (cond 
+             (nil? j-ff) sff
+             (empty? ff) (assoc ff \A 5)
+             :else (sort compare-frequencies-v2 (update ff (first (last sff)) #(+ j-ff %1))))
+        
+        sf (cond 
+             (nil? j-sf) ssf
+             (empty? sf) (assoc sf \A 5)
+             :else (sort compare-frequencies-v2 (update sf (first (last ssf)) (fnil #(+ j-sf %1) 5))))
+        
+        r (cond 
+            (> (second (last ff)) (second (last sf))) 1
+            (< (second (last ff)) (second (last sf))) -1
+            (< (count ff) (count sf)) 1
+            (> (count ff) (count sf)) -1
+            (> (get cards-v2 (nth f 0)) (get cards-v2 (nth s 0))) 1
+            (< (get cards-v2 (nth f 0)) (get cards-v2 (nth s 0))) -1
+            
+            (> (get cards-v2 (nth f 1)) (get cards-v2 (nth s 1))) 1
+            (< (get cards-v2 (nth f 1)) (get cards-v2 (nth s 1))) -1
+            
+            
+            (> (get cards-v2 (nth f 2)) (get cards-v2 (nth s 2))) 1
+            (< (get cards-v2 (nth f 2)) (get cards-v2 (nth s 2))) -1
+            
+            
+            (> (get cards-v2 (nth f 3)) (get cards-v2 (nth s 3))) 1
+            (< (get cards-v2 (nth f 3)) (get cards-v2 (nth s 3))) -1
+            
+            (> (get cards-v2 (nth f 4)) (get cards-v2 (nth s 4))) 1
+            (< (get cards-v2 (nth f 4)) (get cards-v2 (nth s 4))) -1
+            
+            (> (get cards-v2 (nth f 5)) (get cards-v2 (nth s 5))) 1
+            (< (get cards-v2 (nth f 5)) (get cards-v2 (nth s 5))) -1
+            :else 0
+            )]
+    
+    r))
+
+(assert (= 5905 (->> puzzles-t
+                  (map parse-puzzle)
+                  (sort compare-puzzle-v2)
+                  (map second)
+                  calc)))
+
+(assert (= 248029057 (->> puzzles
+                       (map parse-puzzle)
+                       (sort compare-puzzle-v2)
+                       (map second)
+                       calc)))
 
