779feff71f339746c70d7c6c0c2ec3c5aabd6c1c
Maxim Tsoy <mtsoy1990@gmail.com>
2023-12-09 11:47:04 +0600

    day 9

--- src/aoc2023/d9.clj
+++ src/aoc2023/d9.clj
@@ -10,5 +10,39 @@
     (mapv #(Integer/parseInt %1) r)))
 
 
-(->> history-t
-  (map parse-array))
+(defn calc [m] 
+  (loop [m m r (last m)]
+    (if (= (count(frequencies m)) 1)
+      r
+      (let [new-m (map-indexed (fn [idx itm] 
+                                 (- itm (get m idx))) (next m))]
+        (recur (vec new-m) (+ r (last new-m)))))))
+
+
+(assert (= 18 (calc [0 3 6 9 12 15])))
+(assert (= -3 (calc [15 12 9 6 3 0])))
+(assert (= 68 (calc [10  13  16  21  30  45])))
+
+(assert (= 1 (count (frequencies [3 3 3 3 3]))))
+
+(assert (= [15 12 9 6 3 0] (vec (reverse [0 3 6 9 12 15]))))
+
+
+(assert (= 114 (->> history-t
+                 (map parse-array)
+                 (map calc)
+                 (apply +))))
+
+(assert (= 2 (->> history-t
+               (map (comp calc vec reverse parse-array))
+               (apply +))))
+
+
+(assert (= 1789635132 (->> history
+                        (map parse-array)
+                        (map calc)
+                        (apply +))))
+
+(assert (= 913 (->> history
+                 (map (comp calc vec reverse parse-array))
+                 (apply +))))
