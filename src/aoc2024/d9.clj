af016bd88b911cdfc7523a6782c838dfef333855
Maxim Tsoy <mtsoy1990@gmail.com>
2024-12-09 22:17:37 +0900

    refactoring

--- src/aoc2024/d9.clj
+++ src/aoc2024/d9.clj
@@ -37,36 +37,36 @@
       :else (recur (next m) (inc i) id (conj r {:f (first m)})))))
 
 
-(defn find-free-space [m s]
+(defn find-free-space [m s find-free-space-cache]
   (let [c (count m)]
-    (loop [i 0]
+    (loop [i (find-free-space-cache s 0)]
       (if (= i c) 
-        nil
-        (let [g (get m i)]
-          (cond 
-            (contains? g :r) (recur (inc i))
-            (>= ^long (g :f) s) i
-            :else (recur (inc i))))))))
+        [nil (assoc find-free-space-cache s c)]
+        (let [g (get m i)
+              t (g :f)]
+          (if 
+            (and (some? t) (>= t s)) [i (assoc find-free-space-cache s i)]
+            (recur (inc i))))))))
 
 (defn compact-disk [m] 
-  (loop [m m r (dec (count m))] 
+  (loop [m m r (dec (count m)) find-free-space-cache {}] 
     (cond 
       (= r 0) m
-      (contains? (get m r) :f) (recur m (dec r))
+      (contains? (get m r) :f) (recur m (dec r) find-free-space-cache)
       :else (let [f (get m r)
-                  i (find-free-space m (f :r))]
+                  [i find-free-space-cache] (find-free-space m (f :r) find-free-space-cache)]
         (if (nil? i)
-          (recur m (dec r))
+          (recur m (dec r) find-free-space-cache)
           (let [reminder (- ((get m i) :f) (f :r))]
             (cond 
-              (> i r) (recur m (dec r))
+              (> i r) (recur m (dec r) find-free-space-cache)
               (= reminder 0) (recur (-> m
                                       (assoc r {:f (f :r)})
-                                      (assoc i {:r (f :r) :id (f :id)})) (dec r))
+                                      (assoc i {:r (f :r) :id (f :id)})) (dec r) find-free-space-cache)
               :else (recur (-> m
                              (assoc r {:f (f :r)})
                              (assoc i {:f reminder})
-                             (vec-insert i {:r (f :r) :id (f :id)})) (dec r)))))))))
+                             (vec-insert i {:r (f :r) :id (f :id)})) (dec r) find-free-space-cache))))))))
 
 (defn calculate-2 [m]
   (let [c (count m)]
@@ -75,9 +75,9 @@
         r
       	(let [f (get m i)]
           (cond 
-            (contains? f :f) (recur m (inc i) (+ ir (f :f)) r)
+            (contains? f :f) (recur m (inc i) (+ ir ^long (f :f)) r)
             (= 0 (f :r)) (recur m (inc i) ir r)
-            :else (recur (update-in m [i :r] dec) i (inc ir) (+ r (* ir (f :id))))))))))
+            :else (recur (update-in m [i :r] dec) i (inc ir) (+ r (* ir ^long (f :id))))))))))
 
 (assert (= 2858 (->> test-disk-map
   (mapv #(- (int %) 48))
@@ -90,7 +90,6 @@
   parse-disk-map
   compact-disk
   calculate-2)))
-
 
 (time (->> disk-map
   (mapv #(- (long %) 48))
@@ -108,3 +107,6 @@
   calculate-2))
   (println (prof/stop))
   (println "end profiling")))
+
+(set! *warn-on-reflection* true)
+
