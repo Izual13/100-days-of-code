26c561175175e8eca77d15cc51718ce8c1074e39
Maxim Tsoy <mtsoy1990@gmail.com>
2024-12-31 04:31:20 +0900

    day 17 part 2 2024

--- src/aoc2024/d17.clj
+++ src/aoc2024/d17.clj
@@ -12,7 +12,6 @@
         p (str/split p #",")]
     [(Integer/parseInt a) (Integer/parseInt b) (Integer/parseInt c) (mapv Integer/parseInt p)]))
 
-
 (defn calculate [[a b c p]]
   (loop [i 0 a a b b c c r []]
     (if (= i (count p)) 
@@ -29,22 +28,14 @@
                             6 c
                             7 nil)]
         (cond 
-          
-          (= 0 opcode) (recur (+ 2 i) (int (/ a (math/pow 2 combo-operand))) b c r)
+          (= 0 opcode) (recur (+ 2 i) (bit-shift-right a combo-operand) b c r)
           (= 1 opcode) (recur (+ 2 i) a (bit-xor b operand) c r)
           (= 2 opcode) (recur (+ 2 i) a (mod combo-operand 8) c r)
           (= 3 opcode) (if (= 0 a) (recur (+ 2 i) a b c r) (recur operand a b c r))
           (= 4 opcode) (recur (+ 2 i) a (bit-xor b c) c r)
           (= 5 opcode) (recur (+ 2 i) a b c (conj r (mod combo-operand 8)))
-          (= 6 opcode) (recur (+ 2 i) a (int (/ a (math/pow 2 combo-operand))) c r)
-          (= 7 opcode) (recur (+ 2 i) a b (int (/ a (math/pow 2 combo-operand))) r))))))
-
-
-(calculate [0 0 9 [2 6]])
-(calculate [10 0 0 [5,0,5,1,5,4]])
-(calculate [2024 0 0 [0,1,5,4,3,0]])
-(calculate [0 29 0 [1 7]])
-(calculate [0 2024 43690 [4 0]])
+          (= 6 opcode) (recur (+ 2 i) a (bit-shift-right a combo-operand) c r)
+          (= 7 opcode) (recur (+ 2 i) a b (bit-shift-right a combo-operand) r))))))
 
 
 (assert (= {:a 0, :b 0, :c 0, :r [4 6 3 5 6 3 5 2 1 0]} (->> test-info
@@ -55,4 +46,25 @@
 (assert (= {:a 0, :b 6, :c 3, :r [6 0 6 3 0 2 3 1 6]} (->> info
   parse-info
   calculate)))
+
+(defn eq [f p c]
+  (let [f' (vec (take-last c f))
+        p' p]
+    (loop [i 0]
+      (cond 
+        (= i c) true
+        (= (get f' i) (get p' i)) (recur (inc i))
+        :else false))))
+
+(defn hack[p]
+  (loop [i 1 cs 1]
+    (let [tmp ((calculate [i 0 0 p]) :r)]
+      (cond 
+        (= p tmp) i
+        (eq p tmp cs) (recur (bit-shift-left i 3) (inc cs))
+        :else (recur (inc i) cs)))))
+
+(assert (= 117440 (hack [0,3,5,4,3,0])))
+
+(assert (= 236539226447469 (hack [2 4 1 3 7 5 0 3 1 5 4 4 5 5 3 0])))
 
