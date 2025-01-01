205c61cf02387119ce6f7e21d14e3448e666089b
Maxim Tsoy <mtsoy1990@gmail.com>
2025-01-02 06:04:11 +0900

    refactoring

--- src/aoc2024/d16.clj
+++ src/aoc2024/d16.clj
@@ -20,27 +20,26 @@
 (defn find-neighbours [i j m cd cr v]
   (let [check (fn [i' j' r' d'] (and (not (= (get-in m [i' j']) \#)) (or (nil? (v [i' j' d'])) (< r' (v [i' j' d'])))))]
     (case cd
-      \^ (for [[oy ox r nd] [[-1 0 0 \^][1 0 2000 \v][0 1 1000 \>][0 -1 1000 \<]]
+      \^ (for [[oy ox r d] [[-1 0 0 \^][1 0 2000 \v][0 1 1000 \>][0 -1 1000 \<]]
                :let [i' (+ i oy)
                      j' (+ j ox)
                      r' (+ cr r 1)]
-               :when (check i' j' r' nd)] [i' j' nd r'])
-      \v (for [[oy ox r nd] [[-1 0 2000 \^][1 0 0 \v][0 1 1000 \>][0 -1 1000 \<]]
+               :when (check i' j' r' d)] [i' j' d r'])
+      \v (for [[oy ox r d] [[-1 0 2000 \^][1 0 0 \v][0 1 1000 \>][0 -1 1000 \<]]
                :let [i' (+ i oy)
                      j' (+ j ox)
                      r' (+ cr r 1)]
-               :when (check i' j' r' nd)] [i' j' nd r'])
-      \> (for [[oy ox r nd] [[-1 0 1000 \^][1 0 1000 \v][0 1 0 \>][0 -1 2000 \<]]
+               :when (check i' j' r' d)] [i' j' d r'])
+      \> (for [[oy ox r d] [[-1 0 1000 \^][1 0 1000 \v][0 1 0 \>][0 -1 2000 \<]]
                :let [i' (+ i oy)
                      j' (+ j ox)
                      r' (+ cr r 1)]
-               :when (check i' j' r' nd)] [i' j' nd r'])
-      \< (for [[oy ox r nd] [[-1 0 1000 \^][1 0 1000 \v][0 1 2000 \>][0 -1 0 \<]]
+               :when (check i' j' r' d)] [i' j' d r'])
+      \< (for [[oy ox r d] [[-1 0 1000 \^][1 0 1000 \v][0 1 2000 \>][0 -1 0 \<]]
                :let [i' (+ i oy)
                      j' (+ j ox)
                      r' (+ cr r 1)]
-               :when (check i' j' r' nd)] [i' j' nd r'])
-    )))
+               :when (check i' j' r' d)] [i' j' d r']))))
 
 (defn dijkstra [m]
   (let [[si sj] (utils/find-start m \S)
@@ -55,30 +54,6 @@
               points' (disj points [i j d r])
               tmp (v [i j d])]
           (recur (apply conj points' neighbours) (if (or (nil? tmp) (< r tmp)) (assoc v [i j d] r) v)))))))
-
-
-(disj #{[1 23]} [1 3])
-
-(->> test-maze2
-  (mapv vec)
-  dijkstra)
-
-(time (->> test-maze2
-  (mapv vec)
-  dijkstra))
-
-
-(->> maze
-  (mapv vec)
-  dijkstra)
-
-(compare 1 2)
-
-(sorted-set-by (fn [a b] (compare a b)) [[1 2 3 4] [4 5 6 7]])
-
-(->> test-maze
-  (mapv vec)
-  dijkstra)
 
 
 (assert (= 7036 (->> test-maze
@@ -125,13 +100,10 @@
                                  (let [[oi oj od] (first p)
                                        cv (v [i j d])
                                        tmp (v [(+ i oi) (+ j oj) od])]
-                                   ; (if (nil? tmp) 1 (println [i j d] "->" [(+ i oi) (+ j oj) od] cv tmp))
                                    (cond
                                      (nil? tmp) (recur (next p) r)
                                      (or (= 1 (- cv tmp)) 
-                                       (= 1001 (- cv tmp))
-                                       ; (= 2001 (- cv tmp))
-                                       ) (recur (next p) (conj r [(+ i oi) (+ j oj) od]))
+                                       (= 1001 (- cv tmp))) (recur (next p) (conj r [(+ i oi) (+ j oj) od]))
                                      :else (recur (next p) r))))))]
     (loop [points (find-min v ei ej) r 0 v #{}]
       (cond 
@@ -139,7 +111,6 @@
         (contains? v (first points)) (recur (next points) r v)
         :else (let [[i j d] (first points)
                     n (neighbours i j d)]        
-                ; (println "n" n)
                 (recur (apply conj (next points) n) (+ r (count n)) (conj v [i j d])))))))
 
 (assert (= 45 (->> test-maze
