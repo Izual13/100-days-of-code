874d1b492d6a57b892f5ee1b9912767897488798
Maxim Tsoy <mtsoy1990@gmail.com>
2025-01-03 05:03:15 +0900

    day 19 part 2 2024

--- src/aoc2024/d19.clj
+++ src/aoc2024/d19.clj
@@ -31,10 +31,39 @@
       (let [[isPossible cache'] (check-design patterns (first d) cache)]
         (recur (next d) cache' (if isPossible (inc r) r))))))
 
-(->> test-designs
+(assert (= 6 (->> test-designs
   parse-designs
-  check-designs)
+  check-designs)))
 
-(->> designs
+(assert (= 355 (->> designs
   parse-designs
-  check-designs)
+  check-designs)))
+
+
+(defn check-design2 [patterns design cache]
+  (cond 
+    (contains? cache design) [(cache design) cache]
+    (= ""  design) [1 (assoc cache "" 1)]
+    :else (loop [p patterns cache cache r 0]
+            (cond 
+              (empty? p) [r (assoc cache design r)]
+              (= design (first p)) (recur (next p) (assoc cache design 1) (inc r))
+              (str/starts-with? design (first p)) (let [[r' cache'] (check-design2 patterns (subs design (count (first p))) cache)]
+                                                    (recur (next p) cache' (+ r' r)))
+              :else (recur (next p) cache r)))))
+
+
+(defn check-designs2 [[patterns designs]]
+  (loop [d designs cache {} r 0] 
+    (if (empty? d) 
+      r
+      (let [[result cache'] (check-design2 patterns (first d) cache)]
+        (recur (next d) cache' (+ result r))))))
+
+(assert (= 16 (->> test-designs
+  parse-designs
+  check-designs2)))
+
+(assert (= 732978410442050 (->> designs
+  parse-designs
+  check-designs2)))
